85660c070c6c85766a994ec8c91a9f5cdaff9f09
Kezimana Aime Angelo <kezangelo@gmail.com>
2023-08-26 16:23:42 +0200

    first commit

---
+++ src/router/index.js
@@ -0,0 +1,32 @@
+import { createRouter, createWebHistory } from "vue-router";
+// import HomePage from '/src/views/HomePage.vue'
+
+const routes = [
+//   {
+//     path: "/",
+//     name: "Home",
+//     component: HomePage,
+//   },
+//   {
+//     path: "/about",
+//     name: "About",
+//     component: () => import("/src/views/about.vue"),
+//   },
+//   {
+//     path: "/posts",
+//     name: "AllPosts",
+//     component: () => import("/src/views/AllPosts.vue"),
+//   },
+//   {
+//     path: "/posts/:id",
+//     name: "GetPost",
+//     component: () => import("/src/views/GetPost.vue"),
+//   },
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+
+export default router;
