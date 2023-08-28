604c199ea7616f2d0525ba4778377964903e354d
Kezimana Aime Angelo <kezangelo@gmail.com>
2023-08-28 10:49:04 +0200

    fix: Use Hash Mode instead of History Mode

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHashHistory } from "vue-router";
 import HomePage from '/src/views/HomePage.vue'
 
 const routes = [
@@ -25,7 +25,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes,
 });
 
