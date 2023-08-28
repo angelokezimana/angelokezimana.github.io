c0f82a99f9eb69dc547ef22e99442f2211d67e85
Kezimana Aime Angelo <kezangelo@gmail.com>
2023-08-28 21:49:43 +0200

    chores: manual chunk

--- vite.config.js
+++ vite.config.js
@@ -1,36 +1,30 @@
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
-import Markdown from "unplugin-vue-markdown/vite";
-import hljs from "highlight.js";
+import { defineConfig } from "vite"
+import vue from "@vitejs/plugin-vue"
+import Markdown from "unplugin-vue-markdown/vite"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue({
-      include: [/\.vue$/, /\.md$/],
-    }),
-    Markdown({
-      markdownItOptions: {
-        html: true,
-        linkify: true,
-        typographer: true,
-        highlight(str, lang) {
-          if (lang && hljs.getLanguage(lang)) {
-            try {
-              return (
-                '<pre class="hljs"><code>' +
-                hljs.highlight(str, { language: lang, ignoreIllegals: true })
-                  .value +
-                "</code></pre>"
-              );
-            } catch (__) {}
-          }
-        },
-      },
-    }),
-  ],
-  base: "/",
-  build: {
-    sourcemap: true,
-  },
-});
+	plugins: [
+		vue({
+			include: [/\.vue$/, /\.md$/],
+		}),
+		Markdown(),
+	],
+	base: "/",
+	build: {
+		sourcemap: true,
+		rollupOptions: {
+			output: {
+				manualChunks(id) {
+					if (id.includes("node_modules")) {
+						return id
+							.toString()
+							.split("node_modules/")[1]
+							.split("/")[0]
+							.toString()
+					}
+				},
+			},
+		},
+	},
+})
