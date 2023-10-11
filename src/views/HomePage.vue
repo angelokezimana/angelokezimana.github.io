953178e059be5e38892d1b40c07fd53fab90787c
Kezimana Aime Angelo <kezangelo@gmail.com>
2023-10-11 21:55:01 +0200

    fix: vite for production
    Rollup failed to resolve import "pics/kezimana.jpg" from "/home/runner/work/angelokezimana.github.io/angelokezimana.github.io/src/views/HomePage.vue".

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -13,7 +13,7 @@
 		<div class="text-center">
 			<div class="flex justify-center mb-4">
 				<img
-					src="pics/kezimana.jpg"
+					src="/pics/kezimana.jpg"
 					alt="Image"
 					class="shadow rounded-full align-middle border-none w-72 h-72"
 				/>
@@ -35,7 +35,7 @@
 			</p>
 
 			<a
-				href="assets/cv.pdf"
+				href="/assets/cv.pdf"
 				class="mr-2 px-7 py-3 md:px-9 md:py-4 font-medium md:font-semibold bg-gray-700 text-gray-50 text-sm rounded-md hover:bg-gray-50 hover:text-gray-700 transition ease-linear duration-500"
 				target="_blank"
 				>Get my CV</a
